c3dcfc8b3b0a4021e454ed51530680805011ba70
Edgar Santos <edgarrps@gmail.com>
2023-04-02 16:05:38 -0400

    Fix directory error

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import fs, { PathLike, ReadStream } from 'fs'
+import Link from 'next/link'
 
 const getPostMetadata = () => {
   const folder = 'posts/'
-  const files = fs.readFileSync(folder)
-  const mdPosts = files.filter((file: any) => file.endsWith('.md'))
-  const slugs = mdPosts.map((file: any) => file.replace('md', ''))
+  const files = fs.readdirSync(folder)
+  const mdPosts = files.filter((file) => file.endsWith('.md'))
+  const slugs = mdPosts.map((file) => file.replace('md', ''))
   return slugs
 }
 export default function Home() {
   const postMetadata = getPostMetadata()
-  const postPreviews = postMetadata.map((slug) => { <div><h2>{slug}</h2></div> })
+  const postPreviews = postMetadata.map((slug) => {
+    <div>
+      <Link href={`./posts/${slug}`}>
+        <h2>{slug}</h2>
+      </Link>
+    </div>
+  })
   return (<>{postPreviews}</>)
 }
