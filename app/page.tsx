68829cd4b55f3562be94c6a2c2e56ae342d5f140
Edgar Santos <edgarrps@gmail.com>
2023-04-02 15:56:54 -0400

    slug posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+import fs, { PathLike, ReadStream } from 'fs'
 
+const getPostMetadata = () => {
+  const folder = 'posts/'
+  const files = fs.readFileSync(folder)
+  const mdPosts = files.filter((file: any) => file.endsWith('.md'))
+  const slugs = mdPosts.map((file: any) => file.replace('md', ''))
+  return slugs
+}
 export default function Home() {
-  return (<h1>Blog content</h1>)
+  const postMetadata = getPostMetadata()
+  const postPreviews = postMetadata.map((slug) => { <div><h2>{slug}</h2></div> })
+  return (<>{postPreviews}</>)
 }
