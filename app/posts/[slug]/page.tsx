3cae2c071854bb9594169bdb7eeecb45c9b0eccd
Edgar Santos <edgarrps@gmail.com>
2023-04-03 19:53:51 -0400

    print slug

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,7 +2,7 @@ const PostPage = (props: any) => {
     const slug = props.params.slug
     return (
         <p>
-            <h1>Post!</h1>
+            <h1>Post! {slug}</h1>
         </p>
     )
 }
