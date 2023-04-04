d92e894206e54eb90258fcfbf5bc38e0009b3385
Edgar Santos <edgarrps@gmail.com>
2023-04-03 20:04:30 -0400

    getPostContent function created)

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,5 +1,14 @@
+import fs from 'fs'
+
+const getPostContent = (slug: string) => {
+    const file = `posts/${slug}.md`
+    const content = fs.readFileSync(file, 'utf8')
+    return content
+}
 const PostPage = (props: any) => {
     const slug = props.params.slug
+    const content = getPostContent(slug)
+
     return (
         <p>
             <h1>Post! {slug}</h1>
