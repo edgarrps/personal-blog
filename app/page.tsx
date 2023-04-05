1ab977e093c38be3743a6fb20f35ac61ba2880a0
edgarrps <edgarrps@gmail.com>
2023-04-05 03:01:51 -0400

    improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
-import fs from 'fs'
-import matter from 'gray-matter'
+import getPostMetadata from '@/components/getPostMetadata'
 import Link from 'next/link'
-import { PostMetadata } from '@/types/PostMetadata'
 
-const getPosts = (): PostMetadata[] => {
-    const files = fs.readdirSync('posts/')
-    const markdownPosts = files.filter((file) => file.endsWith('.md'))
-    const posts = markdownPosts.map((fileName) => {
-        const fileContents = fs.readFileSync(`posts/${fileName}`, 'utf8')
-        const matterResult = matter(fileContents)
-
-        return {
-            title: matterResult.data.title,
-            date: matterResult.data.date,
-            subtitle: matterResult.data.subtitle,
-            slug: fileName.replace('.md', '')
-        }
-    })
-    return posts
-}
 
 export default function Home() {
-    const postMetadata = getPosts()
+    const postMetadata = getPostMetadata()
     const postPreviews = postMetadata.map((post) => (
         <div>
             <Link href={`/posts/${post.slug}`}>
