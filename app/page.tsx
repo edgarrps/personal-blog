2e52aa0ec67ba7234b6109b1bf85fe1a599d32a9
edgarrps <edgarrps@gmail.com>
2023-04-04 20:16:03 -0400

    use matter lib to view description of posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
 import fs from 'fs'
+import matter from 'gray-matter'
 import Link from 'next/link'
 
 const getPosts = () => {
     const files = fs.readdirSync('posts/')
     const markdownPosts = files.filter((file) => file.endsWith('.md'))
-    const slugs = markdownPosts.map((file) => file.replace('.md', ''))
-    return slugs
+    const posts = markdownPosts.map((fileName) => {
+        const fileContents = fs.readFileSync(`posts/${fileName}`, 'utf8')
+        const matterResult = matter(fileContents)
+        return {
+            tittle: matterResult.data.title,
+            date: matterResult.data.date,
+            subtitle: matterResult.data.subtitle,
+            slug: fileName.replace('.md', '')
+        }
+    })
 }
 
 export default function Home() {
     const postMetadata = getPosts()
-    const postPreviews = postMetadata.map((slug) => (
+    const postPreviews = postMetadata.map((slug:any) => (
         <div>
             <Link href={`/posts/${slug}`}>
                 <h2>{slug}</h2>
